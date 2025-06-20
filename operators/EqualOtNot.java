b06bf7fb215f77928a42cce76bf788a5f761cfed
satya179 <teja64062@gmail.com>
2025-06-20 11:53:49 +0530

    Create EqualOtNot.java

---
+++ operators/EqualOtNot.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class EqualOrNot{
+    public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	if(a==b){
+	    System.out.println("Equal");
+	}
+        else{
+	    System.out.println("Not Equal");
+	}
+    }
+}
