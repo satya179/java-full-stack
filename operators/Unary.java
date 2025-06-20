68b4861d88bae9fc08b5ab12e288da52c30efd2a
satya179 <teja64062@gmail.com>
2025-06-20 11:57:28 +0530

    Create Unary.java

---
+++ operators/Unary.java
@@ -0,0 +1,12 @@
+class Unary{
+    public static void main(String[] args)
+    {
+	int a=50;
+	int b=80;
+	System.out.println(a++);
+	System.out.println(++a);
+
+	System.out.println(b++);
+	System.out.println(++b);
+    }
+}
