dce2d8e185407f00b1d61344c12b4790058152ab
satya179 <teja64062@gmail.com>
2025-06-20 11:55:29 +0530

    Create Relational.java

---
+++ operators/Relational.java
@@ -0,0 +1,13 @@
+class Relational{
+    public static void main(String[] args)
+    {
+	int a=20;
+	int b=30;
+	System.out.println(a==b);
+	System.out.println(a!=b);
+	System.out.println(a>b);
+	System.out.println(a<b);
+	System.out.println(a>=b);
+	System.out.println(a<=b);
+    }
+}
